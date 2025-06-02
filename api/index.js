2c82d47aede1d9efbc6c96b6e92741730205f837
Risfandi Lase <risfandilase7865@gmail.com>
2025-06-02 22:54:37 +0700

    test

--- api/index.js
+++ api/index.js
@@ -1,26 +1,26 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import mockJobs from '../mockJobs.js';
 
 
 const app = express();
 
 // app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// app.get('/jobs', (req, res) => {
-//   const { title, location } = req.query;
-//   let filteredJobs = mockJobs;
-//   if (title) {
-//     filteredJobs = filteredJobs.filter(job => job.title.toLowerCase().includes(title.toLowerCase()));
-//   }
-//   if (location) {
-//     filteredJobs = filteredJobs.filter(job => job.location.toLowerCase().includes(location.toLowerCase()));
-//   }
-//   res.json(filteredJobs);
-// });
+app.get('/jobs', (req, res) => {
+  const { title, location } = req.query;
+  let filteredJobs = mockJobs;
+  if (title) {
+    filteredJobs = filteredJobs.filter(job => job.title.toLowerCase().includes(title.toLowerCase()));
+  }
+  if (location) {
+    filteredJobs = filteredJobs.filter(job => job.location.toLowerCase().includes(location.toLowerCase()));
+  }
+  res.json(filteredJobs);
+});
 
 // app.post('/jobs', (req, res) => {
 //   const newJob = req.body;
