89989e9f15670f637462fb7afecb015a34748189
Risfandi Lase <risfandilase7865@gmail.com>
2025-06-02 23:01:26 +0700

    add

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import mockJobs from '../mockJobs.js';
+import cors from 'cors';
 
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
