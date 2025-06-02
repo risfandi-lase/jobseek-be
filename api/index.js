b2d6ddcd1ffce974c7d7b384c96e73107ce41975
Risfandi Lase <risfandilase7865@gmail.com>
2025-06-02 22:53:35 +0700

    test

--- api/index.js
+++ api/index.js
@@ -3,55 +3,54 @@ import cors from 'cors';
 import mockJobs from '../mockJobs.js';
 
 
-const express = require("express");
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get('/jobs', (req, res) => {
-  const { title, location } = req.query;
-  let filteredJobs = mockJobs;
-  if (title) {
-    filteredJobs = filteredJobs.filter(job => job.title.toLowerCase().includes(title.toLowerCase()));
-  }
-  if (location) {
-    filteredJobs = filteredJobs.filter(job => job.location.toLowerCase().includes(location.toLowerCase()));
-  }
-  res.json(filteredJobs);
-});
+// app.get('/jobs', (req, res) => {
+//   const { title, location } = req.query;
+//   let filteredJobs = mockJobs;
+//   if (title) {
+//     filteredJobs = filteredJobs.filter(job => job.title.toLowerCase().includes(title.toLowerCase()));
+//   }
+//   if (location) {
+//     filteredJobs = filteredJobs.filter(job => job.location.toLowerCase().includes(location.toLowerCase()));
+//   }
+//   res.json(filteredJobs);
+// });
 
-app.post('/jobs', (req, res) => {
-  const newJob = req.body;
-  // Generate a new unique ID (simple increment based on array length)
-  newJob.id = (mockJobs.length + 1).toString();
-  mockJobs.push(newJob);
-  res.status(201).json(newJob);
-});
+// app.post('/jobs', (req, res) => {
+//   const newJob = req.body;
+//   // Generate a new unique ID (simple increment based on array length)
+//   newJob.id = (mockJobs.length + 1).toString();
+//   mockJobs.push(newJob);
+//   res.status(201).json(newJob);
+// });
 
-// Bookmark a job
-app.post('/jobs/:id/bookmark', (req, res) => {
-  const jobId = req.params.id;
-  const job = mockJobs.find(j => j.id === jobId);
-  if (!job) {
-    return res.status(404).json({ message: 'Job not found' });
-  }
-  job.is_bookmarked = true;
-  res.json({ message: 'Job bookmarked', job });
-});
+// // Bookmark a job
+// app.post('/jobs/:id/bookmark', (req, res) => {
+//   const jobId = req.params.id;
+//   const job = mockJobs.find(j => j.id === jobId);
+//   if (!job) {
+//     return res.status(404).json({ message: 'Job not found' });
+//   }
+//   job.is_bookmarked = true;
+//   res.json({ message: 'Job bookmarked', job });
+// });
 
-// Unbookmark a job
-app.post('/jobs/:id/unbookmark', (req, res) => {
-  const jobId = req.params.id;
-  const job = mockJobs.find(j => j.id === jobId);
-  if (!job) {
-    return res.status(404).json({ message: 'Job not found' });
-  }
-  job.is_bookmarked = false;
-  res.json({ message: 'Job unbookmarked', job });
-});
+// // Unbookmark a job
+// app.post('/jobs/:id/unbookmark', (req, res) => {
+//   const jobId = req.params.id;
+//   const job = mockJobs.find(j => j.id === jobId);
+//   if (!job) {
+//     return res.status(404).json({ message: 'Job not found' });
+//   }
+//   job.is_bookmarked = false;
+//   res.json({ message: 'Job unbookmarked', job });
+// });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
